0cd15b28dc9794817d89050c57daf92ad9376b58
Pablo <pablofalons5@gmail.com>
2021-04-28 05:54:16 -0500

    agregado index.js git brach master

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ const Mercado = 'COLOMBIA';
 // funcion para obterner info de tienda según una direccion dada
 
 async function obtenerTiendaCercaDir(CodigoRegion, tipoOrden, NombreCalle, CalleNumero = ' ', Ciudad = ' ') {
-  const response = await fetch(`${API_URL}/store-locator-international/locate/store?regionCode=${CodigoRegion}&type=${tipoOrden}&Street=${NombreCalle}${CalleNumero}&StreetName=${NombreCalle}&Name=&StreetNumber=${CalleNumero}&City=${Ciudad}&Region=${CodigoRegion}&streetAddress1=${NombreCalle}${CalleNumero}&g=1`, {
+  const url = `${API_URL}/store-locator-international/locate/store?regionCode=${CodigoRegion}&type=${tipoOrden}&Street=${NombreCalle}${CalleNumero}&StreetName=${NombreCalle}&Name=&StreetNumber=${CalleNumero}&City=${Ciudad}&Region=${CodigoRegion}&streetAddress1=${NombreCalle}${CalleNumero}&g=1`;
+  const response = await fetch(url, {
     headers: {
       Accept: 'application/vnd.com.dominos.ecommerce.store-locator.response+json;version=1.2',
       Market: `${Mercado}`,
@@ -22,7 +23,8 @@ async function obtenerTiendaCercaDir(CodigoRegion, tipoOrden, NombreCalle, Calle
 
 // funcion para obeter info de tienda segun ID tienda
 async function obtenerTiendaInfo(storeID) {
-  const response = await fetch(`${API_URL}/power/store/${storeID}/profile`, {
+  const url = `${API_URL}/power/store/${storeID}/profile`;
+  const response = await fetch(url, {
     headers: {
       Accept: 'application/json, text/javascript, */*; q=0.01',
       Market: `${Mercado}`,
@@ -34,7 +36,8 @@ async function obtenerTiendaInfo(storeID) {
 
 // funcion para invocar el menu de una tienda por su ID 🍔
 async function obtenerTiendaMenu(storeID) {
-  const response = await fetch(`${API_URL}/power/store/${storeID}/menu?lang=es&includeAssets=true`, {
+  const url = `${API_URL}/power/store/${storeID}/menu?lang=es&includeAssets=true`;
+  const response = await fetch(url, {
     headers: {
       Accept: 'application/json, text/javascript, */*; q=0.01',
       Market: `${Mercado}`,
@@ -66,16 +69,16 @@ async function obtenerTiendaCercaDelivery(CodigoRegion, tipoOrden, NombreCalle, 
 //     console.log(json);
 //   });
 
-// obtenerTiendaCercaDir('ANT', ' ', 'Calle 46 ', ' ', ' Rionegro')
-//   .then((json) => {
-//     console.log(json.Stores[0]);
-//     // console.log(json);
-//   });
-
-obtenerTiendaMenu('17198')
+obtenerTiendaCercaDir('MAG', ' ', 'Calle 17 20 ', ' ', ' Rodadero')
   .then((json) => {
-    console.log(json);
+    console.log(json.Stores[0]);
+    // console.log(json);
   });
+
+// obtenerTiendaMenu('17198')
+//   .then((json) => {
+//     console.log(json);
+//   });
 
 // obtenerTiendaInfo('17198')
 //   .then((infoTienda) => {
